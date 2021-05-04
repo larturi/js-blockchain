e153b3849423cfa1c53eabad6bed7479891a2cf4
lea.arturi <lea.arturi@gmail.com>
2021-05-04 17:05:21 -0300

    Avances

---
+++ src/service/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+
+import express from 'express';
+import Blockchain from '../blockchain';
+import P2PService from './p2p';
+
+const { HTTP_PORT = 3000 } = process.env;
+const app = express();
+
+const blockchain = new Blockchain();
+const p2pService = new P2PService(blockchain);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/blocks', (req, res) => {
+  res.json(blockchain.blocks);
+});
+
+app.post('/mine', (req, res) => {
+  const { body: { data } } = req;
+  const block = blockchain.addBlock(data);
+
+  p2pService.sync();
+
+  res.json({
+    blocks: blockchain.blocks.length,
+    block,
+  });
+});
+
+app.listen(HTTP_PORT, () => {
+  console.log(`Service HTTP:${HTTP_PORT} listening...`);
+  p2pService.listen();
+});
---
+++ src/service/p2p.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-console */
+
+import WebSocket from 'ws';
+
+const { P2P_PORT = 5000, PEERS } = process.env;
+const peers = PEERS ? PEERS.split(',') : [];
+const MESSAGE = { BLOCKS: 'blocks' };
+
+class P2PService {
+  constructor(blockchain) {
+    this.blockchain = blockchain;
+    this.sockets = [];
+  }
+
+  listen() {
+    const server = new WebSocket.Server({ port: P2P_PORT });
+    server.on('connection', (socket) => this.onConnection(socket));
+
+    peers.forEach((peer) => {
+      const socket = new WebSocket(peer);
+      socket.on('open', () => this.onConnection(socket));
+    });
+
+    console.log(`Service ws:${P2P_PORT} listening...`);
+  }
+
+  onConnection(socket) {
+    console.log('[ws:socket] connected.');
+    this.sockets.push(socket);
+    socket.on('message', (message) => {
+      const { type, value } = JSON.parse(message);
+
+      try {
+        if (type === MESSAGE.BLOCKS) this.blockchain.replace(value);
+      } catch (error) {
+        console.error(`[ws:message] error ${error}`);
+      }
+    });
+
+    const { blockchain: { blocks } } = this;
+
+    socket.send(JSON.stringify({ type: MESSAGE.BLOCKS, value: blocks }));
+  }
+
+  broadcast(type, value) {
+    console.log(`[ws:broadcast] ${type}`);
+    const message = JSON.stringify({ type, value });
+    this.sockets.forEach((socket) => { socket.send(message); });
+  }
+
+  sync() {
+    const { blockchain: { blocks } } = this;
+    this.broadcast(MESSAGE.BLOCKS, blocks);
+  }
+}
+
+export default P2PService;
